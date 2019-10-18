501776a279cf17f30789ad9ecafe9256c727541f
marjorie.armando <marjorie.armando@gmail.com>
2019-10-18 13:55:46 +0200

    add version with one thread per gz file

---
+++ c++/QueueSafe.hpp
@@ -0,0 +1,39 @@
+#include <mutex>
+#include <condition_variable>
+#include <queue>
+
+template<typename T>
+
+class QueueSafe
+{
+	private:
+		std::queue<T> _queue;
+		std::mutex _mutex;
+		std::condition_variable _condition_variable;
+	public:
+		void push_front(T const& data){ _queue.push(data); }
+
+		bool empty()
+		{
+			std::lock_guard<std::mutex> guard(_mutex);
+			return _queue.empty();
+		}
+
+		bool try_pop(T& popped_value)
+		{
+			std::lock_guard<std::mutex> guard(_mutex);
+			if(_queue.empty())
+				return false;
+			
+			popped_value = _queue.front();
+			_queue.pop();
+			return true;
+		}
+		
+		void print_items()
+		{
+			std::cout << "Items in queue : \n";
+			for(unsigned i=0; i<_queue.size(); ++i)
+				std::cout << _queue[i] << std::endl;
+		}
+};

5ca881282fdec90bef631f732a2ed87123e9085e
marjorie.armando <marjorie.armando@gmail.com>
2019-10-21 17:11:44 +0200

    frequences to finish

--- c++/c_calcul_frequences.cpp
+++ c++/c_calcul_frequences.cpp
@@ -11,7 +11,6 @@
 #include <cstdio>
 #include <thread>
 #include "QueueSafe.hpp"
-#include "OccurrencesSafe.hpp"
 
 #define LINE_SIZE 1024
 #define NB_NGRAM 4
@@ -84,9 +83,32 @@ void print_error(string message, string cut_filename)
 	cerr << message << cut_filename << "\n";
 }		
 
+FILE* get_file(unsigned thread_id, string filename)
+{	
+	// concat large_filename + num_cut_files
+	string delimiter = "/";
+	size_t pos = 0;
+	while ((pos = filename.find(delimiter)) != std::string::npos) 
+		filename.erase(0, pos + delimiter.length());
+	filename.pop_back();
+	filename.pop_back();
+	filename.pop_back();
+	filename += "_frequences";
+	filename = "/mnt/j/eng_4grams_frequences_thread/" + filename;
 
-void calcul_occurrences(unsigned thread_id, QueueSafe<string>& queue_filenames, 
-	OccurrencesSafe& occurrences)
+	// open output file
+	FILE* output = fopen(filename.c_str(), "w");
+	if( output == NULL )
+		print_error("Impossible to open the file ", filename);
+	cout << "\tThread " << thread_id << " : " << filename << "\n";
+	cerr << "\tThread " << thread_id << " : " << filename << "\n";
+
+	return output;
+}
+
+
+void calcul_freq(unsigned thread_id, QueueSafe<string>& queue_filenames, 
+	unsigned long long& total_match, unsigned long long& total_volume)
 {	
 	char buffer[LINE_SIZE];
 	string line;
@@ -94,6 +116,10 @@ void calcul_occurrences(unsigned thread_id, QueueSafe<string>& queue_filenames,
 	string delimiter = "\t";
 	string large_filename;
 	unsigned position = 0;
+	unsigned nb_match = 0;
+	unsigned nb_volume = 0;
+	float freq_match = 0;
+	float freq_volume = 0;
 	size_t pos = 0;
 			
 	while( !queue_filenames.empty() )
@@ -107,6 +133,12 @@ void calcul_occurrences(unsigned thread_id, QueueSafe<string>& queue_filenames,
 			print_error("Impossible to open the file ", large_filename);
 			break;	
 		}
+		
+		// open the output file
+		FILE* output = get_file(thread_id, large_filename);
+		if( output == NULL )
+			break;
+					
 		print_ok_safe(thread_id, "start", large_filename);
 								
 		while( fgets(buffer, sizeof(buffer), input) )
@@ -125,10 +157,20 @@ void calcul_occurrences(unsigned thread_id, QueueSafe<string>& queue_filenames,
 				if(position > 4)
 					break;
 				else if(position == 3)
-					occurrences.add_match( stoi( token ) );
+					freq_match = stoi( token ) / ; // converts unsigned long long to double ??
 				else if(position == 4)
-					occurrences.add_volume( stoi( token ) );
+					freq_volume = stoi( token ) / ;
 			}
+			
+			if(position < 3 || position < 4)
+			{
+				cerr << "WARNING mauvaise ligne\n";
+			}
+			else
+			{
+				fprintf(); // print la ligne
+			}
+			
 			memset(buffer, 0, sizeof(buffer));
 		}
 		fclose(input);
@@ -143,35 +185,68 @@ void print_ok_safe(unsigned thread_id, string message, string filename)
 	cerr << "Thread " << thread_id << " " << message << " " << filename << "\n";
 }
 
+void get_total_match(const char* filename, 
+	unsigned long long& total_match)
+{
+	FILE* file = fopen(filename, "r");
+	if( file == NULL )
+	{
+		print_error("Cannot open file ", filename);
+		return -1;
+	}
+	char buffer[CHUNK_SIZE];
+	unsigned long long total_volume;
+	fgets(buffer, sizeof(buffer), file);
+	fscanf(file, "%llu\t%llu", total_match, &total_volume);
+	cout << total_match << " " << total_volume << endl;
+	fclose(file);	
+}
+
+void get_total_volume(const char* filename,
+	unsigned long long& total_volume)
+{
+	FILE* file = fopen(filename, "r");
+	if( file == NULL )
+	{
+		print_error("Cannot open file ", filename);
+		return -1;
+	}
+	unsigned year, nb_1gram, nb_pages, nb_volumes;
+	
+	total_volume = 0;
+	while( fscanf(file, "[^ ]\t%d\t%llu\t%llu\t%llu", &year, &nb_1gram, &nb_pages, &nb_volumes) == 4 )
+	{
+		if( year >= YEAR )
+			total_volume += nb_volumes;
+	}
+	
+	cout << " total volume : " << total_volume << endl;
+	fclose(file);		
+}
 
 int main(int argc, char** argv)
 {
 	QueueSafe<string> queue_filenames;
-	OccurrencesSafe occurrences;
 	unsigned nb_cores = std::thread::hardware_concurrency();
 	vector<thread> threads;
+	unsigned long long total_match, total_volume;
 
 	collect_filenames(queue_filenames);
+	get_total_occurrences(argv[1], total_match);
+	get_total_volume(argv[2], total_volume);
+	
+	
 	auto start = high_resolution_clock::now();
 	for(unsigned i=0; i<nb_cores; ++i)
 	{
 		cout << "create thread " << i+1 << endl;
-		threads.emplace_back( [&]{calcul_occurrences( i+1, queue_filenames, occurrences ); } );
+		threads.emplace_back( [&]{calcul_freq( i+1, queue_filenames, total_match, total_volume ); } );
 	}
 	for(auto& t: threads)
 		t.join();
 	threads.clear();
 	
-	FILE* output = fopen(argv[1], "w");
-	if( output == NULL )
-	{
-		print_error("Cannot open file ", argv[1]);
-		return -1;
-	}
-	fprintf(output, "Total match\tTotal volume\n%llu\t%llu\n", 
-		occurrences.get_total_match(), occurrences.get_total_volume());
-	fclose(output);
-	
+
 	auto stop = high_resolution_clock::now(); 
 	auto duration = duration_cast<std::chrono::minutes>(stop - start);
 	cout << "Time taken : " << endl;

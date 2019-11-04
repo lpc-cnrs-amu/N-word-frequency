fd1b2c572cb656f0c05589ae6fd3b438341b2019
marjorie.armando <marjorie.armando@gmail.com>
2019-11-04 16:21:53 +0100

    converts file dos to unix to execute them in unix env

---
+++ c++/convert_dos_to_unix.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+array=('a_generate_files' 'b_calcul_total_occurrences' 'c_calcul_frequences' 'd_calcul_frequences_tag_grams')
+for i in "${array[@]}";do
+	for file in "$i"/*;do
+		if [[ -f $file ]]; then
+		    dos2unix $file
+		fi
+	done
+done
+
+for file in ./*;do
+	if [[ -f $file ]]; then
+		dos2unix $file
+	fi
+done

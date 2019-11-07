9c4836457a48797e4fcc3d329b3ea0c720630a0d
marjorie.armando <marjorie.armando@gmail.com>
2019-11-07 13:34:27 +0100

    download gz files with thread

--- c++/download_gz_files_thread.cpp
+++ c++/download_gz_files_thread.cpp
@@ -3,14 +3,20 @@
 #include <string>
 #include <regex>
 #include <iostream>
+#include <thread>
+#include <mutex>
 
 using namespace std;
 
 #define LINE_SIZE 10000
 
-bool error_curl(const char* message, const char* arg, CURL *curl, 
+mutex print_mutex;
+mutex read_mutex;
+
+bool error_curl(unsigned thread_id, const char* message, const char* arg, CURL *curl, 
 	FILE* fp, const char* output_filename)
 {
+	std::lock_guard<std::mutex> guard(print_mutex);
 	if(arg != NULL)
 		fprintf(stderr, message, arg);
 	else
@@ -22,8 +28,17 @@ bool error_curl(const char* message, const char* arg, CURL *curl,
 	return false;	
 }
 
+void print_safe(unsigned thread_id, const char* message, const char* arg)
+{
+	std::lock_guard<std::mutex> guard(print_mutex);
+	if( arg != NULL )
+		fprintf(stderr, message, arg);
+	else
+		fprintf(stderr, message);
+}
+
 /* download html page (url) in output_filename */
-bool download_file(const char* output_filename, const char* url)
+bool download_file(unsigned thread_id, const char* output_filename, const char* url)
 {
 	CURL *curl;
 	CURLcode code;
@@ -35,51 +50,58 @@ bool download_file(const char* output_filename, const char* url)
 		fp = fopen(output_filename, "wb");
 		if( fp == NULL )
 		{
-			fprintf(stderr, "Impossible to open %s\n", output_filename);
+			print_safe(thread_id, "Impossible to open %s\n", output_filename);
 			return false;
 		}
-		fprintf(stdout, "Downloading %s...\n", url);
+		print_safe(thread_id, "Downloading %s...\n", url);
 		
 		code = curl_easy_setopt(curl, CURLOPT_URL, url);
 		if(code != CURLE_OK) 
-			return error_curl("Failed to set URL %s\n", url, curl, 
+			return error_curl(thread_id, "Failed to set URL %s\n", url, curl, 
 				fp, output_filename);
 		
 		code = curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, NULL);
 		if(code != CURLE_OK) 
-			return error_curl("Failed to set CURLOPT_WRITEFUNCTION\n", 
+			return error_curl(thread_id, "Failed to set CURLOPT_WRITEFUNCTION\n", 
 				NULL, curl, fp, output_filename);
 		
 		code = curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
 		if(code != CURLE_OK) 
-			return error_curl("Failed to set output file %s\n", 
+			return error_curl(thread_id, "Failed to set output file %s\n", 
 				output_filename, curl, fp, output_filename);
 		
 		code = curl_easy_perform(curl);
 		if(code != CURLE_OK) 
-			return error_curl("Failed to perform curl\n", NULL, curl, 
+			return error_curl(thread_id, "Failed to perform curl\n", NULL, curl, 
 				fp, output_filename);
 		
 		curl_easy_cleanup(curl);
 		fclose(fp);
 	}  
 	else
-		return error_curl("Failed to create CURL connection\n", 
+		return error_curl(thread_id, "Failed to create CURL connection\n", 
 			NULL, curl, fp, output_filename);
 
 	return true;
 }
 
-bool search_urls(const char* html_filename, string output_folder, string lang, 
-	string nb_ngrams)
+bool read_one_line(FILE* fp, char* buffer)
 {
-	FILE *fp = fopen(html_filename, "r");
-	if( fp == NULL )
-	{
-		fprintf(stderr, "Impossible to open %s\n", html_filename);
+	std::lock_guard<std::mutex> guard(read_mutex);
+	if ( fgets(buffer, sizeof(buffer), fp) == NULL )
 		return false;
-	}
-	
+	return true;
+}
+
+bool end_of_file(FILE* fp)
+{
+	std::lock_guard<std::mutex> guard(read_mutex);
+	return feof(fp);
+}
+
+bool search_urls(unsigned thread_id, FILE* fp, 
+	string output_folder, string lang, string nb_ngrams)
+{	
     regex pattern_url("http://storage\\.googleapis\\.com/books/ngrams/books/googlebooks-"+
 		lang+"-all-"+nb_ngrams+"gram-20120701-.*\\.gz");
 
@@ -93,9 +115,12 @@ bool search_urls(const char* html_filename, string output_folder, string lang,
 	string output_filename;
 	string url;
 	
-	while( fgets(buffer, sizeof(buffer), fp) )
+	while( !end_of_file(fp) )
 	{
 		line = buffer;
+		
+		if( !read_one_line(fp, buffer) )
+			break;
 		
 		// find a correct url
 		if( regex_search(line, match_url, pattern_url) )
@@ -105,26 +130,41 @@ bool search_urls(const char* html_filename, string output_folder, string lang,
 			{
 				output_filename = match_filename[0];
 				output_filename = output_folder + output_filename;
-				if( !download_file(output_filename.c_str(), url.c_str()) )
-					continue;	
+				if( !download_file(thread_id, output_filename.c_str(), url.c_str()) )
+					continue;
+				print_safe(thread_id, "Finish %s\n", url.c_str());
 			}
 			else
 				fprintf(stderr, "Didn't find the output filename in match %s\n", url.c_str());
 		}
 	}
-	fclose(fp);
-	return true;
+	
 }
 
 int main(void)
 {
 	const char* html_filename = "downloaded_dataset.html";
-	if( !download_file(html_filename, 
+	if( !download_file(1, html_filename, 
 		"http://storage.googleapis.com/books/ngrams/books/datasetsv2.html") )
 		return -1;
 	
-	if( !search_urls(html_filename, "", "fre", "4") )
+	FILE *fp = fopen(html_filename, "r");
+	if( fp == NULL )
+	{
+		fprintf(stderr, "Impossible to open %s\n", html_filename);
 		return -1;
+	}
+	
+	
+	unsigned nb_cores = std::thread::hardware_concurrency();
+	vector<thread> threads;
+	
+	for(unsigned i=0; i<nb_cores; ++i)
+		threads.emplace_back( [&]{search_urls( i+1, fp, "", "fre", "4"); } );
+		
+	for(auto& t: threads)
+		t.join();
+	threads.clear();
 	
     return 0;
 }
